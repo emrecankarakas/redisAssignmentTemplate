79cbe9ee7e94c57850bc43599c17126e25da021e
tugberk.arkose <tugberk.arkose@aizon.ai>
2022-12-27 11:19:18 +0100

    add redis script

---
+++ checkRedis.py
@@ -0,0 +1,7 @@
+import redis
+
+r = redis.Redis(host="localhost", port=6379, db=0)
+p = r.pubsub()
+p.subscribe("my-first-channel", "my-second-channel", ...)
+r.publish("my-first-channel", "Hello World!")
+p.get_message()
