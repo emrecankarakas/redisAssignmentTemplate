26d1b011e42fe82f07a215c6f6c8374c372ac63b
tugberk.arkose <tugberk.arkose@aizon.ai>
2022-12-27 11:20:39 +0100

    fix: redis script

--- checkRedis.py
+++ checkRedis.py
@@ -2,6 +2,6 @@ import redis
 
 r = redis.Redis(host="localhost", port=6379, db=0)
 p = r.pubsub()
-p.subscribe("my-first-channel", "my-second-channel", ...)
+p.subscribe("my-first-channel")
 r.publish("my-first-channel", "Hello World!")
 p.get_message()
